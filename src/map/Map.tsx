f278db411a120f7ec0a72c504c3373a637a28f35
Lair Júnior <lairjr@outlook.com>
2020-05-02 19:04:18 -0400

    Display google map

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Map.css";
+import GoogleMapReact from "google-map-react";
 
 function Map() {
-  return <div>Map</div>;
+  return (
+    <div
+      style={{
+        height: "300px", // Height is controlled by the flex box, it needs to have some value due google maps
+        width: "100%",
+      }}
+    >
+      <GoogleMapReact
+        defaultCenter={{
+          lat: 59.95,
+          lng: 30.33,
+        }}
+        defaultZoom={11}
+        yesIWantToUseGoogleMapApiInternals
+      />
+    </div>
+  );
 }
 
 export default Map;
