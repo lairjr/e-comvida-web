ef6c069df650415fc8cdde2834e91368e85d8405
Lair Júnior <lairjr@outlook.com>
2020-05-02 18:47:47 -0400

    Add map route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./common/NavBar";
+import Map from "./map/Map";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <Map />
     </div>
   );
 }
