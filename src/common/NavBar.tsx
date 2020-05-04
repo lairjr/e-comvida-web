88119b33e0e817c8f29033a02af8d7d366b13399
Lair Júnior <lairjr@outlook.com>
2020-05-03 22:43:51 -0400

    Add mateial navbar

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import "./NavBar.css";
+import { AppBar } from "@material-ui/core";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function NavBar() {
-  return <nav>NavBar</nav>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">News</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default NavBar;
