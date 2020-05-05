71fe78ac015f6f5b896d6ff1767a468659c378b8
Lair Júnior <lairjr@outlook.com>
2020-05-04 20:59:12 -0400

    Add menu on menu burger

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toogleIsMenuOpen = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" aria-label="menu">
+        <IconButton onClick={toogleIsMenuOpen} edge="start" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6">News</Typography>
+
+        <Drawer open={isMenuOpen} onClose={toogleIsMenuOpen}>
+          <List>
+            <ListItem button key="map">
+              <ListItemText primary="Mapa" />
+            </ListItem>
+
+            <ListItem button key="list">
+              <ListItemText primary="Lista" />
+            </ListItem>
+          </List>
+        </Drawer>
+
+        <Typography variant="h6">Fechados pela vida</Typography>
       </Toolbar>
     </AppBar>
   );
