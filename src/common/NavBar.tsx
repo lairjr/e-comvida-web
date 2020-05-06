d3f5879c797c4ddb5672c6c53fa05226bda3adae
Lair Júnior <lairjr@outlook.com>
2020-05-06 00:24:25 -0400

    Adjust app bar to use menu hambuger only for mobile

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AppBar } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,6 +9,9 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+import "./NavBar.css";
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,9 +20,11 @@ function NavBar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton onClick={toogleIsMenuOpen} edge="start" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
+        <Hidden mdUp>
+          <IconButton onClick={toogleIsMenuOpen} edge="start" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
 
         <Drawer open={isMenuOpen} onClose={toogleIsMenuOpen}>
           <List>
@@ -37,7 +42,19 @@ function NavBar() {
           </List>
         </Drawer>
 
-        <Typography variant="h6">Fechados pela vida</Typography>
+        <Typography variant="h6" className="title">
+          Fechados pela vida
+        </Typography>
+
+        <Hidden smDown>
+          <Link to="/list">
+            <Button>Empresas</Button>
+          </Link>
+
+          <Link to="/map">
+            <Button>Mapa</Button>
+          </Link>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
