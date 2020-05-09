1ef7a6d8a84aa7a14b3123a5607da9c62a78d2d7
Lair Júnior <lairjr@outlook.com>
2020-05-08 21:04:58 -0400

    Read data from database

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,37 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import List from "./pages/list/List";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { ReactReduxFirebaseProvider } from "react-redux-firebase";
-import firebase from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+import firebase from "./firebase";
 
 function App() {
+  firebase
+    .firestore()
+    .collection("companies")
+    .get()
+    .then((companies: any) => {
+      console.log(companies.docs);
+    });
   return (
     <Provider store={store}>
-      <ReactReduxFirebaseProvider
-        {...{ config: {}, dispatch: store.dispatch, firebase: firebase }}
-      >
-        <Router>
-          <div className="app">
-            <NavBar />
+      <Router>
+        <div className="app">
+          <NavBar />
 
-            <Switch>
-              <Route exact path="/">
-                <List />
-              </Route>
+          <Switch>
+            <Route exact path="/">
+              <List />
+            </Route>
 
-              <Route exact path="/list">
-                <List />
-              </Route>
+            <Route exact path="/list">
+              <List />
+            </Route>
 
-              <Route exact path="/map">
-                <Map />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </ReactReduxFirebaseProvider>
+            <Route exact path="/map">
+              <Map />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 }
