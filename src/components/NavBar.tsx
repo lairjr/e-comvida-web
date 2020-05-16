e10dd60ace54f600868fa89a4a8f4dbc0f6a6ac7
Lair Júnior <lairjr@outlook.com>
2020-05-16 04:01:24 -0400

    Adjust drawer

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,15 +7,32 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 import Hidden from "@material-ui/core/Hidden";
 import "./NavBar.css";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import logo from "../assets/logo.png";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      backgroundColor: theme.palette.primary.dark,
+    },
+    list: {
+      width: 250,
+    },
+    listItem: {
+      color: theme.palette.primary.contrastText,
+    },
+  })
+);
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toogleIsMenuOpen = () => setIsMenuOpen(!isMenuOpen);
+  const classes = useStyles();
 
   return (
     <Container
@@ -32,23 +49,34 @@ function NavBar() {
             <MenuIcon />
           </IconButton>
 
-          <Drawer open={isMenuOpen} onClose={toogleIsMenuOpen}>
-            <List>
+          <Drawer
+            open={isMenuOpen}
+            onClose={toogleIsMenuOpen}
+            PaperProps={{ className: classes.drawer }}
+          >
+            <List className={classes.list}>
               <ListItem button key="list">
                 <Link to="/list">
-                  <ListItemText primary="Estabelecimentos" />
+                  <ListItemText
+                    primary="Estabelecimentos"
+                    className={classes.listItem}
+                  />
                 </Link>
               </ListItem>
+
+              <Divider />
 
               <ListItem button key="map">
                 <Link to="/map">
-                  <ListItemText primary="Mapa" />
+                  <ListItemText primary="Mapa" className={classes.listItem} />
                 </Link>
               </ListItem>
 
+              <Divider />
+
               <ListItem button key="about">
                 <Link to="/about">
-                  <ListItemText primary="About" />
+                  <ListItemText primary="Sobre" className={classes.listItem} />
                 </Link>
               </ListItem>
             </List>
